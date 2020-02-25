320b40bfa7d246bb2a463e1d831fcbff250d2b91
MahanteshR <41316852+MahanteshR@users.noreply.github.com>
2020-02-25 17:22:11 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,21 @@
+import sqlite3 as sql
+
+
+def insertUser(username, password):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username, password))
+    con.commit()
+    con.close()
+
+
+def retrieveUsers():
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT username, password FROM users")
+    users = cur.fetchall()
+    con.close()
+    return users
+
+insertUser("Jagruth", "1234")
+print(retrieveUsers())
